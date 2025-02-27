c17426a1cc31098a976f15202ceb68ae928baeab
Renars P <rencis360@gmail.com>
2025-02-27 21:14:04 +0200

    pievienoju daudz frazes, znosanas pugu

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,16 @@
-function myFunction() {
-    document.getElementById("randomQuote").innerHTML = "... BET NAV! Tomēr JavaScript funkcionē!!!";
-
-}
 
 var sakumaFrazes = Array("Kad dzīvē iet grūti un pa ielu iet",
-                  "Ja pa ielu iet ",
                   "Tikko kā redzi - ",
                   "Ja tur ir",
-                  "Nē... ",
+                  "Nē... lūk",
                   "Uzreiz kā redzi - ",
-                  "Ja tavā priekšā ir",
-                  "Ko? Tur ",
+                  "Ko? Tur ir",
+                  "Tu skaties, lūk - ",
+                  "Kas tas ir? Ak - ",
+                  "Vai man rādās - ",
+                  "Tas taču nevar būt - ",
+                  "Vai man rādās - ",
+                  "Skat, skat - "
                   );
 
 
@@ -21,69 +21,136 @@ var lietvardi = Array("noziedznieki, ",
                         "kaķis, ",
                         "cilvēki, ",
                         "soma, ",
-                        "Pēteris, "
+                        "Pēteris, ",
+                        "nu - zirgs, ",
+                        "zivtiņa",
+                        "mana mūža mīlestība...",
+                        "mans mājasdarbs,",
+                        "vārna,",
+                        "manas brokastis,",
+                        "pilnīgi nekas,",
+                        "mana mēneša alga,",
+                        "cīsiņš mīklā,",
+                        "maisiņš, kuru vajg,",
+                        "kapibara,",
+                        "lemurs,",
+                        "gorilla"
                      );
 
 
 var talakasDarbibas = Array("tad ",
-                        "uzreiz ",
-                        "zibenīgi ",
-                        "nekavējoties ",
+                        "zibenīgi, ",
+                        "domādams, kā tas vispār ir iespējams, nekavējoties ",
                         "tad uzreiz ",
                         "saprotot, kas ir noticis,",
                         "ilgi nedomājot,",
-                        "uzmanīgi",
+                        "traki",
                         "fiksi,",
                         "a nu ka",
-                        "nesteidzoties",
-                        "gandrīz paslīdot uz neērti novietotas banāna mizas,"
+                        "gandrīz paslīdot uz neērti novietotas banāna mizas,",
+                        "Un es, reaģējot uz to, ko redzu, savas rokas, vienu gar otru rīvējot, ",
+                        "mierīgi",
+                        "ar troksni",
+                        "OHO, VAU VAU VAU,",
+                        "OOOO JĀĀĀ, ",
+                        "tev uznāk milzīga vēlme - ",
+                        "klusi",
+                        "ar ātrumu 120 km/h,",
+                        "uzreiz kā to sapratu, man kaut kāda balss saka: ",
+                        "man rodas ideja: "
+
+
 );
 
 
 var velTalakasDarbibas = Array(
-                        "saproti, ka tava dzīve nemaz nav tik slikta ",
-                        " noglāsti ",
-                        " jāpārdomā dzīvi ",
-                        " jābēg prom ",
-                        " veido dīvainas sejas ",
-                        "diktē savus noteikumus",
-                        " sauc policiju ",
-                        " jāpārliecinās, ka nesapņo ",
-                        "izsaucies - \"cik tas nežēlīgi!\"",
-                        "vispirms aizej uz tualeti",
-                        " saki: \"Vai tramvajā dzimis? \" "
+                        " noglāstu, ",
+                        " jāpārdomā dzīvi, ",
+                        " bēdz vien prom, ",
+                        " apsver domu veidot dīvainas sejas, ",
+                        "diktē savus noteikumus, paga, ko... ",
+                        "noliec visu pa plauktiņiem",
+                        "pieliec savu dzelzs dūri",
+                        " sauc policiju, ",
+                        " jāpārliecinās, ka nesapņo, ",
+                        "izsaucies - \"cik tas nežēlīgi!\",",
+                        "vispirms aizej aiz stūra, un, un tad, tad, tad tu - ,",
+                        "saki nē,",
+                        "teikt, ka jokus nevajag te veikt",
+                        "saki pie sevis: \"jokus nevajag te veikt \", ",
+                        " no tavas mutes izskan - \"Es tā vairs nedarīšu\"",
+                        "es tūlīt...",
+                        "ieraugi nejauko situāciju šajā saulainajā dienā"
+
 
 );
 
 
-var ekstraDarbibas = Array("un ātri ",
-                        ", kā arī - ",
-                        ", un - ",
+var ekstraDarbibas = Array(
+                        "lūdzu -",
+                        " un - ",
                         " - ",
-                        ", un protams -  ",
-                        ", arī - ",
-                        ", vienkārši",
-                        ", ak, un arī - ",
-                        "nu viss - "
+                        " Ak vai - ",
+                        "uj uj uj - ",
+                        "AHA - ",
+                        "AHA! Tiešām domāji, ka tā būs laba doma? Rekur labāka - ",
+                        "plānu maiņa - ",
+                        "ha, ne-e, ",
+                        "nu gan... ",
+                        "tā tik turpini - ",
+                        "zini ko? - ",
+                        "saki \"Paldies par informāciju\" un",
+                        "tas ir neticami - ",
+                        "ja tā padomā, labāk - ",
+                        "ha, šis man atgādina sapnim kur, ja atceros, ē - "
 );
 
 var papildusDarbibas = Array("ej prom.",
-                            "aizver durvis", 
-                            "jānožēlo sava rīcība",
-                            " piezvani mammai",
-                            "jāzvana mammai",
-                            "izpildi mājasdarbus",
-                            "maksā nodokļus",
-                            " pabaro kaķi",
-                            "tin makšķeres",
-                            "ziņo par noziegumu",
+                            "man tam nav laika, mamma man pankūkas izcepa, atā!",
+                            "aizver durvis.", 
+                            "ko es tikko izdarīju.",
+                            "vai omei par šo visu arī izstāstīt?",
+                            "izpildi mājasdarbus.",
+                            "tin makšķeres.",
+                            "ziņo par noziegumu.",
                             "ziņo par nepieņemamo situāciju, kas ir radusies šajā jaukajā, saulainajā dienā.",
                             "hi hi hi, ha ha ha!",
                             "hi hi hi!",
                             "ho ho ho!",
+                            "oi, he he he he hēē...",
                             "priecīgus Ziemassvētkus!",
-                            "APRIL APRIL",
-                            "nē - tā nav laba doma, piedod, ka es tev tādu teikumu iedevu."
+                            "APRIL APRIL.",
+                            "Nē - tā nav laba doma, piedod, ka es tev tādu teikumu iedevu.",
+                            "Un tā, pasaka beidzās.",
+                            "visi tavi zirgi ir izbēguši no staļļa.",
+                            "saproti, ka tava dzīve nemaz nav tik slikta.",
+                            " O - man ir ideja! Izveidot kaut kādu HTML, CSS un JavaScript lapu, kurā ir visādas muļķības sarakstītas.",
+                            "joks, nekad tā nedari.",
+                            "aizmirsti, ko tev teicu.",
+                            "aizmirsti, to, ko tev teicu.",
+                            "NU viss, viss",
+                            "Atvainojamies par sagādātajām neērtībām.",
+                            "aj, zini, neko.",
+                            "aj, neko.",
+                            "neko",
+                            "nē nē, pietiks, vairs neko.",
+                            "APRIL APRIL, ja kas.",
+                            "bet maizi gan aizmirsu nopirkt.",
+                            "zivs - ah ņam ņam ņam",
+                            "nezināju, ka Maksimā šitādas lietas notiek.",
+                            "atnācu pēc končām, a saņēmu kaut ko citu.",
+                            "nav ko teikt, sapnis bija traks.",
+                            "tā, kur ir manas zāles?",
+                            "KO, KO?? KO TU DARI?",
+                            "KOOOOOOOOO TU DARI????!!??!",
+                            "ak vai vai vai... ko es tikko pateicu...",
+                            "kāpēc man tā bija jādara?",
+                            "kāpēc es to izdarīju?",
+                            "ko es tikko pateicu...",
+                            "[neko]",
+                            "NEKAD VAIRS TĀ NERUNĀ PAR PANKŪKĀM!",
+                            "tiesi tā. Pag, KO?",
+                            "ar Dievu, mans draugs..."
                             );
 
 
@@ -97,4 +164,12 @@ function reveal() {
   document.getElementById('velTalakasDarbibas').innerHTML = velTalakasDarbibas[Math.floor(Math.random() * velTalakasDarbibas.length)];
   document.getElementById('ekstraDarbibas').innerHTML = ekstraDarbibas[Math.floor(Math.random() * ekstraDarbibas.length)];
   document.getElementById('papildusDarbibas').innerHTML = papildusDarbibas[Math.floor(Math.random() * papildusDarbibas.length)];
+}
+
+function troll() {
+  document.getElementById("troll").innerHTML = "Ziņots par teikumu!"
+}
+
+function unTroll() {
+  document.getElementById("troll").innerHTML = "Ziņo par teikumu"
 }
